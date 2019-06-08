4f8cc59afa3e535e06f060b7ca43a42b2480572e
sortegae <oeselma@yahoo.com>
2019-06-08 11:25:27 -0500

    Update preload

--- preload.js
+++ preload.js
@@ -1,13 +1,17 @@
 function preload() {
+	game.stage.backgroundColor = '#285885'; //Color del background en hexadecimal
+    
+	//game.load.image('fondo','https://pbs.twimg.com/media/C3df3qaUEAEW-DD.jpg');
+    
+	game.load.crossOrigin = 'anonymous';
+    
+	game.load.image('platform', 'https://phaser.io/content/tutorials/coding-tips-003/ice-platform.png');
+    
+	game.load.image('snowball','https://vignette.wikia.nocookie.net/clubpenguin/images/0/04/Snowball.png/revision/latest?cb=20140616182803')
 
-    game.stage.backgroundColor = '#85b5e1'; //Color del background en hexadecimal
+	game.load.spritesheet('trump', 'https://3.bp.blogspot.com/-_NVjscKbE7Q/WLB4GtYM6PI/AAAAAAAAIyk/iGm67QmaiV0v0VKNSnG5pzenizWGsbvyQCLcB/s1600/trump_run.png', 100, 100, 24);
 
-    game.load.baseURL = 'http://examples.phaser.io/assets/'; //Direccion de donde sacaremos las imagenes
-    game.load.crossOrigin = 'anonymous';
-
-    game.load.image('player1', 'sprites/phaser-dude.png'); //Cargar la imagen para el sprite del player
-    game.load.image('platform', 'sprites/platform.png');  //Cargar la imagen para el sprite de las plataformas
-
-    game.load.image('player2', 'sprites/phaser-ship.png');  //Cargar la para el sprite de las plataformas
+	game.load.spritesheet('pen', 'https://scontent.fmex5-1.fna.fbcdn.net/v/t34.0-12/28233744_1924343230909354_1883905372_n.png?oh=921bc8c264673fbaf24da83af66e9e59&oe=5AA482B7', 41, 42, 64);
+    
 
 }
